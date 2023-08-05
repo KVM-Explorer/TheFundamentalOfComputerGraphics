d0d7ba12c21591f2525ad603953df679c8e2b742
KVM <wfcyywh1225@126.com>
2023-08-05 11:07:36 +0800

    修复基础库运算bug

--- Common/color.h
+++ Common/color.h
@@ -1,48 +1,45 @@
 #pragma once
 
 #include <stdint.h>
-template <typename T>
-struct Color{
+template <typename T> struct Color {
     T r;
     T g;
     T b;
     T a;
-    Color():r(0),g(0),b(0),a(0){};
-    Color(T r,T g,T b,T a = 1):
-        r(r),g(g),b(b),a(a){};
-    Color(const Color & x)
-        :r(x.r),g(x.g),b(x.b),a(x.a){}
+    Color() : r(0), g(0), b(0), a(0){};
+    Color(T r, T g, T b, T a = 1) : r(r), g(g), b(b), a(a){};
+    Color(const Color &x) : r(x.r), g(x.g), b(x.b), a(x.a) {}
 
-    Color& operator+(const Color& u)
-    {
-        r += u.r;
-        g += u.g;
-        b += u.b;
-        a += u.a;
-        return *this;
+    Color &operator+(const Color &u) {
+        Color ret;
+        ret.r = r + u.r;
+        ret.g = g + u.g;
+        ret.b = b + u.b;
+        ret.a = a + u.a;
+        return ret;
     }
-    Color& operator/(const float& u){
-        r /= u;
-        g /= u;
-        b /= u;
-        a /= u;
-        return *this;
-    }   
-    Color & operator*(const float & u)
-    {
-        r *= u;
-        g *= u;
-        b *= u;
-        a *= u;
-        return *this;
+    Color &operator/(const float &u) {
+        Color ret;
+        ret.r = r / u;
+        ret.g = g / u;
+        ret.b = b / u;
+        ret.a = a / u;
+        return ret;
     }
-    Color & operator*(const Color & u)
-    {
-        r *= u.r;
-        g *= u.g;
-        b *= u.b;
-        a *= u.a;
-        return *this;
+    Color &operator*(const float &u) {
+        Color ret;
+        ret.r = r * u;
+        ret.g = g * u;
+        ret.b = b * u;
+        ret.a = a * u;
+        return ret;
+    }
+    Color &operator*(const Color &u) {
+        Color ret;
+        ret.r = r * u.r;
+        ret.g = g * u.g;
+        ret.b = b * u.b;
+        ret.a = a * u.a;
+        return ret;
     }
 };
-
--- Common/vec.h
+++ Common/vec.h
@@ -7,31 +7,34 @@ struct vec3{
     vec3() = default;
     vec3(T x,T y,T z):x(x),y(y),z(z){};
     vec3(const vec3<T> &v):x(v.x),y(v.y),z(v.z){};
-    
 
-    vec3 & operator-(const vec3 &u) {
-        x -= u.x;
-        y -= u.y;
-        z -= u.z;
-        return *this;
+
+    vec3 &operator-(const vec3 &u) {
+        vec3 ret;
+        ret.x = x -  u.x;
+        ret.y = y -  u.y;
+        ret.z = z -  u.z;
+        return ret;
     }
     vec3 & operator*(const float & u)
     {
-        x *= u;
-        y *= u;
-        z *= u;
-        return *this;
+        vec3 ret;
+        ret.x = x * u;
+        ret.y = y * u;
+        ret.z = z * u;
+        return ret;
     }
-    vec3& operator+(const vec3 &u){
-        x += u.x;
-        y += u.y;
-        z += u.z;
-        return *this;
+    vec3 &operator+(const vec3 &u) {
+        vec3 ret;
+        ret.x = x + u.x;
+        ret.y = y + u.y;
+        ret.z = z + u.z;
+        return ret;
     }
 };
 
 template<typename T>
-T dot(const vec3<T>& a,const vec3<T>& b){
+T dot(const vec3<T>& a,const vec3<T>& b) {
     return a.x*b.x + a.y *b.y +a.z*b.z;
 }
 
